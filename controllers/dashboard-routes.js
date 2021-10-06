61120f5173d84b4d9223957b00679d67fead8275
Ehsan Khosravi <ekhosr@gmail.com>
2021-10-06 19:56:22 -0400

    push the tested code

---
+++ controllers/dashboard-routes.js
@@ -0,0 +1,66 @@
+const router = require("express").Router();
+const sequelize = require("../config/connection");
+const { Post, User, Comment } = require("../models");
+const withAuth = require("../utils/auth");
+
+router.get("/", withAuth, (req, res) => {
+  Post.findAll({
+    where: {
+      user_id: req.session.user_id,
+    },
+    order: [["created_at", "DESC"]],
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["id", "username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      const posts = dbPostData.map((post) => post.get({ plain: true }));
+      res.render("dashboard", {
+        posts,
+        loggedIn: req.session.loggedIn,
+        title: "dashboard",
+        layout: "dashboard-main",
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+router.get("/edit/:id", withAuth, (req, res) => {
+  Post.findOne({
+    where: {
+      id: req.params.id,
+    },
+    order: [["created_at", "DESC"]],
+    attributes: ["id", "title", "content", "created_at"],
+    include: [
+      {
+        model: User,
+        attributes: ["username"],
+      },
+    ],
+  })
+    .then((dbPostData) => {
+      if (!dbPostData) {
+        res.status(404).json({ message: "no post found with this id" });
+        return;
+      }
+      const post = dbPostData.get({ plain: true });
+      res.render("edit-post", {
+        post,
+        loggedIn: true,
+      });
+    })
+    .catch((err) => {
+      console.log(err);
+      res.status(500).json(err);
+    });
+});
+
+module.exports = router;
---
+++ public/javascript/add-post.js
@@ -0,0 +1,106 @@
+async function addPost(event) {
+    event.preventDefault();
+  
+    const title = document.querySelector('input[name="post-title"]').value;
+    const content = document.querySelector('textarea[name="post-content"]').value;
+    var newPostContainerEL = document.querySelector("#new-post-container");
+    var dashboardContainerEl = document.querySelector("#dashboard-container");
+  
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response);
+    if (response.ok) {
+      document.location.replace("/dashboard");
+      newPostContainerEL.classList.add("hide");
+      dashboardContainerEl.classList.remove("hide");
+    } else {
+      alert(response.statusText);
+    }
+  }
+  document.querySelector("#new-post-form").addEventListener("submit", addPost);
+  
+  function newFormHandler(event) {
+    console.log("hi");
+    var newPostContainerEL = document.querySelector("#new-post-container");
+    var dashboardContainerEl = document.querySelector("#dashboard-container");
+  
+    newPostContainerEL.classList.remove("hide");
+    dashboardContainerEl.classList.add("hide");
+  }
+  
+  document
+    .querySelector("#add-post-btn")
+    .addEventListener("click", newFormHandler);
+  
+  const editbtns = document.querySelectorAll("button.edit-review-btn");
+  for (editbtn of editbtns) {
+    editbtn.addEventListener("click", function (event) {
+      event.preventDefault();
+  
+      const testUserId = $(this)[0].id.split(" ")[1];
+    });
+  }
+  
+  var reviewContent;
+  var userId;
+  var post_id;
+  
+  $("#staticBackdrop").on("show.bs.modal", function (event) {
+    var myVal = $(event.relatedTarget)[0].id.split(" ")[0];
+    var reviewContent = $(event.relatedTarget)[0].value;
+    userId = $(event.relatedTarget)[0].id.split(" ")[0].replace("user-", "");
+    post_id = $(event.relatedTarget)[0].id.split(" ")[1].replace("edit-", "");
+    $("#edit-post-content").text(reviewContent);
+  });
+  
+  async function editReviewHandler(event) {
+    event.preventDefault();
+  
+    console.log(userId);
+    console.log(post_id);
+  
+    const reviewContent = document.querySelector("#edit-post-content").value;
+    console.log(reviewContent);
+  
+    const response = await fetch(`/api/posts`, {
+      method: "POST",
+      body: JSON.stringify({
+        title,
+        content,
+      }),
+      headers: {
+        "Content-Type": "application/json",
+      },
+    });
+    console.log(response);
+    if (response.ok) {
+      document.location.replace("/dashboard");
+      newPostContainerEL.classList.add("hide");
+      dashboardContainerEl.classList.remove("hide");
+    } else {
+      alert(response.statusText);
+    }
+  }
+  
+  document
+    .querySelector('button[name="save-btn-modal"]')
+    .addEventListener("click", editReviewHandler);
+  
+  function deletereviewtest(event) {
+    event.preventDefault();
+  
+    console.log($(this)[0].id.split(" ")[1].replace("edit-", ""));
+  }
+  
+  document
+    .querySelector(".delete-review-btn")
+    .addEventListener("click", deletereviewtest);
+  
